329cfd8333b45e57e5cd8822764c738bdde4a22c
Danjo Syu <>
2024-11-17 23:16:31 +0900

    sidebarのコンポーネント作成

---
+++ html/sidebar-company.php
@@ -0,0 +1,58 @@
+<aside class="home-company-content-sidebar">
+  <div class="home-company-content-sidebar__inner">
+    <div class="home-company-content-sidebar-new-recruit__inner">
+      <div class="home-company-content-sidebar-new-recruit__title__inner">
+        <h3 class="sidebar-title">新着メッセージ</h3>
+        <a href="new-job-info.php" class="home-company-content-sidebar-new-recruit__view__inner">
+          <span>view all</span>
+          <div class="home-company-content-sidebar-new-recruit__view__arrow"></div>
+        </a>
+      </div>
+      <div class="home-company-content-sidebar-new-recruit__card__inner">
+        <a href="" class="home-company-content-sidebar-new-recruit__card">
+          <time>2024.07.22</time>
+          <h4 class="home-company-content-sidebar-new-recruit__card__companyName">この文字はダミーですこの文字・・・</h4>
+          <span class="home-company-content-sidebar-new-recruit__card__text">株式会社ANCa</span>
+          <span class="home-company-content-sidebar-new-recruit__card__text">佐治佳奈子（000-0000-0000）</span>
+        </a>
+        <a href="" class="home-company-content-sidebar-new-recruit__card">
+          <time>2024.07.22</time>
+          <h4 class="home-company-content-sidebar-new-recruit__card__companyName">この文字はダミーですこの文字・・・</h4>
+          <span class="home-company-content-sidebar-new-recruit__card__text">株式会社ANCa</span>
+          <span class="home-company-content-sidebar-new-recruit__card__text">佐治佳奈子（000-0000-0000）</span>
+        </a>
+        <a href="" class="home-company-content-sidebar-new-recruit__card">
+          <time>2024.07.22</time>
+          <h4 class="home-company-content-sidebar-new-recruit__card__companyName">この文字はダミーですこの文字・・・</h4>
+          <span class="home-company-content-sidebar-new-recruit__card__text">株式会社ANCa</span>
+          <span class="home-company-content-sidebar-new-recruit__card__text">佐治佳奈子（000-0000-0000）</span>
+        </a>
+      </div>
+    </div>
+    <div class="home-company-content-sidebar-people__inner">
+      <div class="home-company-content-sidebar-people__title__inner">
+        <h3 class="sidebar-title">応募選考管理</h3>
+      </div>
+      <div class="home-company-content-sidebar-people__card__inner">
+        <div class="home-company-content-sidebar-people__card">
+          <h4 class="home-company-content-sidebar-people__card__event__name">過去の決定人数決定人数の表示</h4>
+          <div class="home-company-content-sidebar-people__card__event__people">
+            <span>200</span>人
+          </div>
+        </div>
+        <div class="home-company-content-sidebar-people__card">
+          <h4 class="home-company-content-sidebar-people__card__event__name">過去の決定人数決定人数の表示</h4>
+          <div class="home-company-content-sidebar-people__card__event__people">
+            <span>200</span>人
+          </div>
+        </div>
+        <div class="home-company-content-sidebar-people__card">
+          <h4 class="home-company-content-sidebar-people__card__event__name">過去の決定人数決定人数の表示</h4>
+          <div class="home-company-content-sidebar-people__card__event__people">
+            <span>200</span>人
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</aside>
---
+++ html/sidebar.php
@@ -0,0 +1,89 @@
+<aside class="home-content-sidebar">
+  <div class="home-content-sidebar__inner">
+    <div class="home-content-sidebar-new-recruit__inner">
+      <div class="home-content-sidebar-new-recruit__title__inner">
+        <h3 class="sidebar-title">新着求人案内</h3>
+        <a href="new-job-info.php" class="home-content-sidebar-new-recruit__view__inner">
+          <span>view all</span>
+          <div class="home-content-sidebar-new-recruit__view__arrow"></div>
+        </a>
+      </div>
+      <div class="home-content-sidebar-new-recruit__card__inner">
+        <a href="new-job-detail.php" class="home-content-sidebar-new-recruit__card">
+          <time>2024.07.22</time>
+          <h4 class="home-content-sidebar-new-recruit__card__companyName">会社名ああああああ</h4>
+          <span class="home-content-sidebar-new-recruit__card__text">営業、販売</span>
+          <span class="home-content-sidebar-new-recruit__card__text">営業、販売</span>
+        </a>
+        <a href="new-job-detail.php" class="home-content-sidebar-new-recruit__card">
+          <time>2024.07.22</time>
+          <h4 class="home-content-sidebar-new-recruit__card__companyName">会社名ああああああ</h4>
+          <span class="home-content-sidebar-new-recruit__card__text">営業、販売</span>
+          <span class="home-content-sidebar-new-recruit__card__text">営業、販売</span>
+        </a>
+        <a href="new-job-detail.php" class="home-content-sidebar-new-recruit__card">
+          <time>2024.07.22</time>
+          <h4 class="home-content-sidebar-new-recruit__card__companyName">会社名ああああああ</h4>
+          <span class="home-content-sidebar-new-recruit__card__text">営業、販売</span>
+          <span class="home-content-sidebar-new-recruit__card__text">営業、販売</span>
+        </a>
+      </div>
+    </div>
+    <div class="home-content-sidebar-schedule__inner">
+      <div class="home-content-sidebar-schedule__title__inner">
+        <h3 class="sidebar-title">説明会日程</h3>
+        <a href="session-schedule.php" class="home-content-sidebar-schedule__view__inner">
+          <span>view all</span>
+          <div class="home-content-sidebar-schedule__view__arrow"></div>
+        </a>
+      </div>
+      <div class="home-content-sidebar-schedule__card__inner">
+        <a href="" class="home-content-sidebar-schedule__card">
+          <h4 class="home-content-sidebar-schedule__card__event__name">イベント名</h4>
+          <div class="home-content-sidebar-schedule__card__event__calendar">
+            <img src="assets/images/calendar-icon.png" alt="">
+            <span>9/5 10:00〜 他日程あり</span>
+          </div>
+          <div class="home-content-sidebar-schedule__card__event__place">
+            <img src="assets/images/pin-icon.png" alt="">
+            <span>新潟県 オンライン</span>
+          </div>
+          <div class="home-content-sidebar-schedule__card__event__occupation">
+          <img src="assets/images/screen-icon.png" alt="">
+            <span>営業、エンジニア</span>
+          </div>
+        </a>
+        <a href="" class="home-content-sidebar-schedule__card">
+          <h4 class="home-content-sidebar-schedule__card__event__name">イベント名</h4>
+          <div class="home-content-sidebar-schedule__card__event__calendar">
+            <img src="assets/images/calendar-icon.png" alt="">
+            <span>9/5 10:00〜 他日程あり</span>
+          </div>
+          <div class="home-content-sidebar-schedule__card__event__place">
+            <img src="assets/images/pin-icon.png" alt="">
+            <span>新潟県 オンライン</span>
+          </div>
+          <div class="home-content-sidebar-schedule__card__event__occupation">
+          <img src="assets/images/screen-icon.png" alt="">
+            <span>営業、エンジニア</span>
+          </div>
+        </a>
+        <a href="" class="home-content-sidebar-schedule__card">
+          <h4 class="home-content-sidebar-schedule__card__event__name">イベント名</h4>
+          <div class="home-content-sidebar-schedule__card__event__calendar">
+            <img src="assets/images/calendar-icon.png" alt="">
+            <span>9/5 10:00〜 他日程あり</span>
+          </div>
+          <div class="home-content-sidebar-schedule__card__event__place">
+            <img src="assets/images/pin-icon.png" alt="">
+            <span>新潟県 オンライン</span>
+          </div>
+          <div class="home-content-sidebar-schedule__card__event__occupation">
+          <img src="assets/images/screen-icon.png" alt="">
+            <span>営業、エンジニア</span>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</aside>
